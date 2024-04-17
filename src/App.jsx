4ced05883561fde6a7b3f6ba630b224765add832
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-04-17 10:57:30 +0700

    fixed bug navbar, finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from 'react';
 import './App.css'
 import Navbar from './Components/Navbar'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
-
+  useEffect(() => {
+    AOS.init();
+  })
   return (
     <>
       <Navbar />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,32 +1,49 @@
+import { useState } from 'react';
+import 'animate.css';
 
-export default function Navbar() {
+function Navbar() {
+  const [isNavMenuOpen, setIsNavMenuOpen] = useState(false);
+
+  const toggleNavMenu = () => {
+    setIsNavMenuOpen(!isNavMenuOpen);
+  };
+
   return (
-    <header className="absolute top-0 left-0 z-10 flex items-center w-full bg-transparent">
+    <header className="absolute top-0 left-0 z-10 flex items-center w-full overflow-hidden bg-transparent">
       <div className="container">
         <div className="relative flex items-center justify-between">
           <div className="px-4 animate__animated animate__fadeInLeft">
-            <a href="#home" className="block py-6 font-extrabold text-primary">Exvade.</a>
+            <a href="#home" className="block py-6 font-bold text-primary">Exvade.</a>
           </div>
           <div className="flex items-center px-4">
-            <button id="hamburger" name="hamburger" type="button" className="absolute block right-1 lg:hidden">
+            <button
+              id="hamburger"
+              name="hamburger"
+              type="button"
+              className={`animate__animated animate__fadeInRight absolute block right-4 lg:hidden ${isNavMenuOpen ? 'hamburger-active' : ''}`}
+              onClick={toggleNavMenu}
+            >
               <span className="transition duration-300 ease-in-out origin-top-left hamburger-line"></span>
               <span className="transition duration-300 ease-in-out hamburger-line"></span>
               <span className="transition duration-300 ease-in-out origin-bottom-left hamburger-line"></span>
             </button>
 
-            <nav id="nav-menu" className="hidden absolute py-5 bg-white shadow-lg rounded-lg max-w-[200px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none animate__animated animate__fadeInRight">
+            <nav
+              id="nav-menu"
+              className={`absolute py-5 bg-white shadow-lg rounded-lg max-w-[200px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none animate__animated animate__fadeInRight ${isNavMenuOpen ? '' : 'hidden'}`}
+            >
               <ul className="block lg:flex">
                 <li className="group">
-                  <a href="#home" className="flex py-2 mx-8 text-base text-dark group-hover:text-primary font-medium">Home</a>
+                  <a href="#home" className="flex py-2 mx-8 text-base font-medium text-dark group-hover:text-primary">Home</a>
                 </li>
                 <li className="group">
-                  <a href="#about" className="flex py-2 mx-8 text-base text-dark group-hover:text-primary font-medium">About Me</a>
+                  <a href="#about" className="flex py-2 mx-8 text-base font-medium text-dark group-hover:text-primary">About Me</a>
                 </li>
                 <li className="group">
-                  <a href="#projects" className="flex py-2 mx-8 text-base text-dark group-hover:text-primary font-medium">Projects</a>
+                  <a href="#projects" className="flex py-2 mx-8 text-base font-medium text-dark group-hover:text-primary">Projects</a>
                 </li>
                 <li className="group">
-                  <a href="#contact" className="flex py-2 mx-8 text-base text-dark group-hover:text-primary font-medium">Contact</a>
+                  <a href="#contact" className="flex py-2 mx-8 text-base font-medium text-dark group-hover:text-primary">Contact</a>
                 </li>
               </ul>
             </nav>
@@ -36,3 +53,5 @@ export default function Navbar() {
     </header>
   );
 }
+
+export default Navbar;
