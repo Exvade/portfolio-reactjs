64c9d762b8d818ca096e75a3c54e6ca13942e531
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-04-21 22:06:15 +0700

    hover button

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -15,7 +15,7 @@ export default function Buttons({ menuItems, filterItems, setItems }) {
     <div className="flex px-4 mt-2">
       <button
         className={`p-1 px-2 text-[12px] md:text-[14px] mx-1 font-medium rounded-sm border-primary text-primary border-[1px] ${
-          activeButton === null ? "bg-primary text-white" : ""
+          activeButton === null ? "bg-primary text-white" : "transition-colors duration-500 hover:bg-primary hover:text-white"
         }`}
         onClick={() => {
           setActiveButton(null);
@@ -28,7 +28,7 @@ export default function Buttons({ menuItems, filterItems, setItems }) {
         <button
           key={val}
           className={`p-1 px-2 text-[12px] md:text-[14px] mx-1 font-medium rounded-sm border-primary text-primary border-[1px] ${
-            activeButton === val ? "bg-primary text-white" : ""
+            activeButton === val ? "bg-primary text-white" : "transition-colors duration-500 hover:bg-primary hover:text-white"
           }`}
           onClick={() => handleClick(val)}
         >
--- src/Components/SkillCard.jsx
+++ src/Components/SkillCard.jsx
@@ -4,7 +4,7 @@ export default function SkillCard({item}){
     <div className="grid grid-cols-2 gap-4 px-4 mt-6 lg:grid-cols-4">
       {item.map((val) => (
         <div key={val.id} className={`grid-card group`} style={{ borderColor: val.border }}>
-          <img src={val.logo} className="inline-block w-6 aspect-square"/>
+          <img src={val.logo} className="inline-block w-6"/>
           <h6 className="px-2 text-sm font-semibold transition-all duration-300">{val.title}</h6>
         </div>
       ))}
