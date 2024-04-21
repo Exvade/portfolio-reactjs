09e65f50c8effc6a06f83073768c06762c933c74
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-04-21 18:29:35 +0700

    menambahkan efek bg-primary ketika button filter aktif

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,21 +1,40 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
-import Data from "../Data"
+import { useState } from "react";
+import Data from "../Data";
 
 export default function Buttons({ menuItems, filterItems, setItems }) {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleClick = (val) => {
+    setActiveButton(val);
+    filterItems(val);
+  };
+
   return (
     <div className="flex px-4 mt-2">
-      <button className="p-1 px-2 mx-1 text-[14px] font-medium text-white rounded-sm bg-primary" onClick={() => setItems(Data)}>
+      <button
+        className={`p-1 px-2 text-[12px] md:text-[14px] mx-1 font-medium rounded-sm border-primary text-primary border-[1px] ${
+          activeButton === null ? "bg-primary text-white" : ""
+        }`}
+        onClick={() => {
+          setActiveButton(null);
+          setItems(Data);
+        }}
+      >
         All
       </button>
-      {
-        menuItems.map(val => (
-          <button className="p-1 px-2 text-[14px] mx-1 font-medium text-white rounded-sm bg-primary"
-          onClick={() => filterItems(val)}>
-            {val}
-          </button>
-        ))
-      }
+      {menuItems.map((val) => (
+        <button
+          key={val}
+          className={`p-1 px-2 text-[12px] md:text-[14px] mx-1 font-medium rounded-sm border-primary text-primary border-[1px] ${
+            activeButton === val ? "bg-primary text-white" : ""
+          }`}
+          onClick={() => handleClick(val)}
+        >
+          {val}
+        </button>
+      ))}
     </div>
-  )
+  );
 }
