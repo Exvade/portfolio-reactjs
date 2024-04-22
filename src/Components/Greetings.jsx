1330048099c1c61bd3bba24b166cc52f31e2ba44
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-04-22 22:04:02 +0700

    menambahkan button github

--- src/Components/Greetings.jsx
+++ src/Components/Greetings.jsx
@@ -1,4 +1,5 @@
 import resume from '../file/Deft-Valian-Exanova.pdf'
+import github from '../img/github.png'
 
 export default function Greetings() {
   return (
@@ -7,6 +8,12 @@ export default function Greetings() {
       <h2 className="mb-5 text-lg font-medium text-slate-500 lg:text-2xl">Frontend Engineer</h2>
       <p className="mt-1 mb-10 font-medium leading-relaxed text-slate-400">Welcome to My Personal <span className="font-bold text-dark">Website</span></p>
       <a href={resume} download className="px-6 py-3 text-sm font-semibold text-white transition duration-300 ease-in-out rounded-xl bg-primary hover:shadow-lg hover:opacity-80">Download Resume</a>
+      <div className='w-[115px] mt-7 rounded-xl '>
+      <a href="https://github.com/Exvade" target='_blank' className="flex items-center w-full gap-2 px-4 py-2 text-sm font-semibold text-black transition duration-300 ease-in-out bg-white border-2 border-black rounded-xl hover:shadow-lg hover:opacity-80 hover:bg-black hover:text-white">
+        <img src={github} alt="github png" className='inline w-6 aspect-square'/>
+        <span className='flex items-center pt-[0.5px]'>Github</span> 
+      </a>
+      </div>
     </div>
   )
 }
