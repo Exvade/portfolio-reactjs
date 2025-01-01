bca675b7c73a43665d3155e5633b2064998fd1ed
Deft Valian Exanova <deftvalian2411@gmail.com>
2025-01-01 15:20:05 +0700

    href

--- src/Components/Certification.jsx
+++ src/Components/Certification.jsx
@@ -2,23 +2,23 @@
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
-export default function Certification({item}){
-  useEffect(()=> {
-    Aos.init({duration: 1000})
+export default function Certification({ item }) {
+  useEffect(() => {
+    Aos.init({ duration: 1000 })
   }, [])
-  return(
+  return (
     <div className="grid grid-cols-1 gap-2 px-4 mt-6 lg:grid-cols-4 lg:gap-4">
       {item.map((val) => (
-        <a to={val.link} target="_blank" className="mb-5 overflow-hidden bg-white rounded-md shadow-lg sm:mb-0" data-aos="fade-up"
-        data-aos-anchor-placement="top-bottom" key={val.id}>
-        <img src={val.img} alt={val.alt} className="object-cover object-top w-full aspect-video" />
-        <div className="flex items-center px-2 py-4">
-          <div>
-            <img src={val.logo} alt="" className="w-8 rounded-full aspect-square" />
+        <a href={val.link} target="_blank" className="mb-5 overflow-hidden bg-white rounded-md shadow-lg sm:mb-0" data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom" key={val.id}>
+          <img src={val.img} alt={val.alt} className="object-cover object-top w-full aspect-video" />
+          <div className="flex items-center px-2 py-4">
+            <div>
+              <img src={val.logo} alt="" className="w-8 rounded-full aspect-square" />
+            </div>
+            <p className="px-2 py-2 text-sm font-medium lg:text-base" style={{ color: val.text }} >{val.title}</p>
           </div>
-          <p className="px-2 py-2 text-sm font-medium lg:text-base" style={{color: val.text}} >{val.title}</p>
-        </div>
-      </a>
+        </a>
       ))}
     </div>
   )
